20d001ef0968365160331e7d19ef2afd69039907
Alok <alokp5491@gmail.com>
2026-05-08 13:25:14 +0530

    happp

--- BACKEND/src/controllers/notification.controllers.js
+++ BACKEND/src/controllers/notification.controllers.js
@@ -1,4 +1,4 @@
-import { Notification } from "../models/notification.model.js"
+import { Notification } from "../models/notification.models.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
